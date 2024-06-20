09dfe81e45c2f77d86ae214b22f9bb71711191c5
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-20 16:39:19 -0400

    Crean componentes para el menú y el footer del sitio

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../menu/menu.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule,RouterModule,MenuComponent],
+  imports: [CommonModule,RouterModule,MenuComponent,FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
