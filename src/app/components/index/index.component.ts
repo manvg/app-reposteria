77b200360fcab0db35a8bcba5c3ad8580ed7e569
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-06-20 14:50:51 -0400

    Comienzo migración de código desde proyecto HTML5 a Angular. 1)Se agrega componenete "index" 2)Se agrega componente menú. para ser utilizado en los demás coponentes. 3)Se migra código CSS

---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../menu/menu.component';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [CommonModule,RouterModule,MenuComponent],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss'
+})
+export class IndexComponent {
+
+}
